2b70ead8842f84279863cc33a94deeb1df69b60c
GregoryB-git <92431930+GregoryB-git@users.noreply.github.com>
2026-07-24 15:41:21 +0200

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {} #key=str, value=[val, timestamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = "" #this is the expected return if nothing is found
+        values = self.store.get(key, [])
+
+        #bisearch
+        l, r = 0, len(values) -1
+        while l <= r:
+            m = (l+r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            
+            elif values[m][1] == timestamp:
+                return res
+
+            else:
+                r = m - 1
+
+        return res
+
+        
