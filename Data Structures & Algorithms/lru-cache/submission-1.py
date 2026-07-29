a4cf537144f4e98330ff48cd26a51eb730557b27
GregoryB-git <92431930+GregoryB-git@users.noreply.github.com>
2026-07-29 15:45:56 +0200

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # map the key to nodes
+
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left.next = self.right # finds LRU
+        self.right.prev = self.left   # finds MRU
+
+    def remove(self, node): # removing to the right from left
+        prev = node.prev
+        nxt = node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node): # inserting at right
+        prev = self.right.prev
+        nxt = self.right
+
+        node.next = nxt
+        node.prev = prev
+
+        prev.next = nxt.prev = node
+
+        
+        
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
